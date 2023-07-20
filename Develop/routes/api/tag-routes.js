876956b15c07752435ea7f86782055457283ccf5
Aaron <awitthohn@yahoo.com>
2023-07-20 17:13:18 -0400

    post, update and delete tag working in insomnia

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -50,17 +50,50 @@ router.get('/:id', async (req, res) => {
 });
 
 
-
-router.post('/', (req, res) => {
-  // create a new tag
+ // create a new tag
+ router.post('/', async (req, res) => {
+  try {
+    const tag = await Tag.create(req.body);
+    res.status(200).json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+
+
   // update a tag's name by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
+  router.put('/:id', async (req, res) => {
+    try {
+      const tag = await Tag.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(tag);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+
   // delete on tag by its `id` value
-});
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const tag = await Tag.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(tag);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
